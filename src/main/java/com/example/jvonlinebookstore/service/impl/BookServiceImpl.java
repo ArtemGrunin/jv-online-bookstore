1d597c7d3e6dabb6b48234f9921a6214507831b2
artem_g <tantisravidomina@gmail.com>
2024-06-18 15:19:38 +0300

    [HW-01] Added log info for BookService

--- src/main/java/com/example/jvonlinebookstore/service/impl/BookServiceImpl.java
+++ src/main/java/com/example/jvonlinebookstore/service/impl/BookServiceImpl.java
@@ -5,8 +5,10 @@ import com.example.jvonlinebookstore.repository.BookRepository;
 import com.example.jvonlinebookstore.service.BookService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class BookServiceImpl implements BookService {
@@ -14,11 +16,13 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public Book save(Book book) {
+        log.info("Creating book in DB: {}", book);
         return bookRepository.save(book);
     }
 
     @Override
     public List<Book> findAll() {
+        log.info("Finding all books from DB");
         return bookRepository.findAll();
     }
 }
