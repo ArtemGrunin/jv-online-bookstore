2cc9a5ecb00ed38f98ebaaa877b709e79ec631cb
artem_g <tantisravidomina@gmail.com>
2024-07-01 13:04:55 +0300

    [HW-01] Deleted repository.Added @Repository to JpaRepo

--- src/main/java/com/example/jvonlinebookstore/repository/JpaBookRepository.java
+++ src/main/java/com/example/jvonlinebookstore/repository/JpaBookRepository.java
@@ -2,6 +2,8 @@ package com.example.jvonlinebookstore.repository;
 
 import com.example.jvonlinebookstore.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface JpaBookRepository extends JpaRepository<Book, Long> {
 }
--- src/main/java/com/example/jvonlinebookstore/service/impl/BookServiceImpl.java
+++ src/main/java/com/example/jvonlinebookstore/service/impl/BookServiceImpl.java
@@ -1,28 +1,24 @@
 package com.example.jvonlinebookstore.service.impl;
 
 import com.example.jvonlinebookstore.model.Book;
-import com.example.jvonlinebookstore.repository.BookRepository;
+import com.example.jvonlinebookstore.repository.JpaBookRepository;
 import com.example.jvonlinebookstore.service.BookService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 public class BookServiceImpl implements BookService {
-    private final BookRepository bookRepository;
+    private final JpaBookRepository bookRepository;
 
     @Override
     public Book save(Book book) {
-        log.info("Creating book in DB: {}", book);
         return bookRepository.save(book);
     }
 
     @Override
     public List<Book> findAll() {
-        log.info("Finding all books from DB");
         return bookRepository.findAll();
     }
 }
